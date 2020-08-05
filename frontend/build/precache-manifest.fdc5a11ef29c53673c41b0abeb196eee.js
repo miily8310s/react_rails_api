85a3795d562a453350041f785aff831d268f3dcd
miily8310s <rillakerika@gmail.com>
2020-08-05 21:38:24 +0900

    add build

---
+++ frontend/build/precache-manifest.fdc5a11ef29c53673c41b0abeb196eee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8baaf782d4baa308c18fe3fdf738dfbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00b74eb3ccdd166ca8e1",
+    "url": "/static/css/main.f9e8a586.chunk.css"
+  },
+  {
+    "revision": "bbbec4c2c3af5c0e6df0",
+    "url": "/static/js/2.838b6760.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.838b6760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b74eb3ccdd166ca8e1",
+    "url": "/static/js/main.8be9d6ec.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
